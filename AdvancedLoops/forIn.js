ac26ba3f152fb6dbd3f1cb1b71dc1bcadda0abe2
zikrianbia <zikri4nbia@gmail.com>
2021-01-31 14:12:28 +0700

    addLoops

---
+++ AdvancedLoops/forIn.js
@@ -0,0 +1,14 @@
+/** For In Loops
+ * for in - properties
+ *  Enumerating - Object
+ */
+
+// const car = ['Tesla', 'BMW', 'Porsche'];
+const door = {
+    lamborghini: 2,
+    rollsRoyce: 4
+};
+
+ for (items in door) {
+     console.log(items)
+ };
---
+++ AdvancedLoops/forOf.js
@@ -0,0 +1,21 @@
+/** For of loops
+ *  Iterating - Arrays, Strings
+ */
+
+const car = ['Tesla', 'BMW', 'Porsche'];
+// const door = {
+//     lamborghini: 2,
+//     rollsRoyce: 4
+// };
+//Arrays
+for (item of car) {
+    console.log(item)
+};
+//Strings
+for (item of 'Elon') {
+    console.log(item)
+};
+//tidak bisa ke object
+// for (item of door) {
+//     console.log(item)
+// };
